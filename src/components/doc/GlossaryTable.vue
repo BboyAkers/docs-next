8a213cae39db2d56e856d2e96d3cf02081126ee3
unknown <austin.akers5@gmail.com>
2020-08-30 21:27:15 -0500

    pushing title and header value

---
+++ src/components/doc/GlossaryTable.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Glossary
+      <v-spacer />
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      />
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="glossaryList"
+      :search="search"
+    />
+  </v-card>
+</template>
+
+<script>
+  import * as glossaryJson from '../../data/glossary.json'
+  export default {
+    data: () => ({
+      search: '',
+      headers: [
+        {
+          text: 'Word',
+          align: 'start',
+          sortable: false,
+          value: 'word',
+        },
+        { text: 'Additional Meaning', value: 'additionalMeaning' },
+        { text: 'Common Usage If Component', value: 'commonUsageIfComponent' },
+        { text: 'Definition', value: 'definition' },
+        { text: 'Tags', value: 'tags' },
+      ],
+      glossaryList: glossaryJson,
+    }),
+  }
+
+</script>
